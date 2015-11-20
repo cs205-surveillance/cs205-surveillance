c9258d2c3b17419ee5507ab3da2fefac7eec63f7
1Reinier <reiniermaat@msn.com>
2015-11-20 15:38:25 -0500

    Some code to try-out in driver

--- driver.py
+++ driver.py
@@ -1,2 +1,27 @@
-import pycuda
+# JUST TESTING
 
+import pycuda as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import numpy as np
+
+a = np.random.randn(4, 4)
+a = a.astype(np.float32)
+a_gpu = cuda.mem_alloc(a.nbytes)
+cuda.memcpy_htod(a_gpu, a)
+
+mod = SourceModule("""
+  __global__ void doublify(float *a)
+  {
+    int idx = threadIdx.x + threadIdx.y*4;
+    a[idx] *= 2;
+  }
+  """)
+
+func = mod.get_function("doublify")
+func(a_gpu, block=(4, 4, 1))
+
+a_doubled = np.empty_like(a)
+cuda.memcpy_dtoh(a_doubled, a_gpu)
+print a_doubled
+print a
