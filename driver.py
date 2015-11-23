db25dc1a25617d52b28ae204df4d2bab8b6269d9
1Reinier <reiniermaat@msn.com>
2015-11-23 15:43:40 -0500

    driver update

--- driver.py
+++ driver.py
@@ -6,42 +6,38 @@ import numpy as np
 source = SourceModule(open('run_gaussian_average.cu').read())
 run_gaussian_average = source.get_function('run_gaussian_average')
 
-mu = None
-sig2 = None
-threshold = 1.96
+# Grab one image
+# grabber001 => np.array
+I = np.random.randn(1920, 1080)
 
-for i in range(10):
-    # Grab one image
-    # grabber001 => np.array
+# Copy to device
+I = I.astype(np.float32)
+I_gpu = cuda.mem_alloc(I.nbytes)
+cuda.memcpy_htod(I_gpu, I)
 
-    # Copy to device
-    I = np.random.randn(1920, 1080)
-    I = I.astype(np.float32)
-    I_gpu = cuda.mem_alloc(I.nbytes)
-    cuda.memcpy_htod(I_gpu, I)
+mu = np.zeros_like(I)
+mu_gpu = cuda.mem_alloc(mu.nbytes)
+cuda.memcpy_htod(mu_gpu, mu)
 
-    mu = np.zeros_like(I)
-    mu_gpu = cuda.mem_alloc(mu.nbytes)
-    cuda.memcpy_htod(mu_gpu, mu)
+sig2 = np.ones_like(I)
+sig2_gpu = cuda.mem_alloc(sig2.nbytes)
+cuda.memcpy_htod(sig2_gpu, sig2)
 
-    sig2 = np.ones_like(I)
-    sig2_gpu = cuda.mem_alloc(sig2.nbytes)
-    cuda.memcpy_htod(sig2_gpu, sig2)
+OUT = np.zeros_like(I)
+OUT_gpu = cuda.mem_alloc(OUT.nbytes)
+cuda.memcpy_htod(OUT_gpu, OUT)
 
-    OUT = np.zeros_like(I)
-    OUT_gpu = cuda.mem_alloc(OUT.nbytes)
-    cuda.memcpy_htod(OUT_gpu, OUT)
+# Do algorithm
+run_gaussian_average(cuda.In(I), cuda.InOut(mu), cuda.InOut(sig2), cuda.In(threshold), cuda.Out(OUT))
 
-    # Do algorithm
-    run_gaussian_average(cuda.In(I), cuda.InOut(mu), cuda.InOut(sig2), cuda.In(threshold), cuda.Out(OUT))
+# Copy back
+cuda.memcpy_dtoh(mu_gpu, mu)
+cuda.memcpy_dtoh(sig2_gpu, sig2)
+cuda.memcpy_dtoh(OUT_gpu, OUT)
 
-    # Copy back
-    cuda.memcpy_dtoh(mu_gpu, mu)
-    cuda.memcpy_dtoh(sig2_gpu, sig2)
-    cuda.memcpy_dtoh(OUT_gpu, OUT)
+# Post process
 
-    # Post process
 
-    # Show image, perhaps with pylab
-    print OUT
+# Show image, perhaps with pylab
+print OUT
 
