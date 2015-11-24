f0b22a6cb00bc69d78880644c53cec7e333283cc
Taylor Killian <taylorkillian@g.harvard.edu>
2015-11-24 06:18:50 -0500

    made slight adjustments to driver.py, to actually read in images, convert them to grayscale and set up initial values for mu and sigma2

--- driver.py
+++ driver.py
@@ -2,24 +2,27 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 import numpy as np
+from scipy import misc
 
 source = SourceModule(open('run_gaussian_average.cu').read())
 run_gaussian_average = source.get_function('run_gaussian_average')
 
 # Grab one image
 # grabber001 => np.array
-I = np.random.randn(1920, 1080)
+# I = np.random.randn(1920, 1080)
+I = misc.imread('../thouis/grabber000.ppm',flatten=True)
 
 # Copy to device
 I = I.astype(np.float32)
 I_gpu = cuda.mem_alloc(I.nbytes)
 cuda.memcpy_htod(I_gpu, I)
 
-mu = np.zeros_like(I)
+# mu = np.zeros_like(I)
+mu = np.copy(I)               # As an initialization, set mu to initial image in stack
 mu_gpu = cuda.mem_alloc(mu.nbytes)
 cuda.memcpy_htod(mu_gpu, mu)
 
-sig2 = np.ones_like(I)
+sig2 = np.ones_like(I)        # As an initialization, set variance to 1 for each pixel
 sig2_gpu = cuda.mem_alloc(sig2.nbytes)
 cuda.memcpy_htod(sig2_gpu, sig2)
 
