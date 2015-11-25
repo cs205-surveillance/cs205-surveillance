18d2ccd81d1fc6a3cf3d8691a99fe0bc80d6f4f4
1Reinier <reiniermaat@msn.com>
2015-11-25 14:31:09 -0500

    rpca cuda update

--- rpca_cuda.py
+++ rpca_cuda.py
@@ -12,17 +12,20 @@ shrinker = ElementwiseKernel("float *x, float *z, float tau",
 squared = ElementwiseKernel("float *x, float *z",
                             "z[i] = abs(pow(x[i], 2))") 
 
-def robust_pca(M):
+def robust_pca(D):
     """ 
     Parrallel RPCA using ALM, adapted from https://github.com/nwbirnie/rpca.
-    Takes gpuarrays and returns numpy arrays.
+    Takes and returns numpy arrays
     """
+    M = gpuarray.to_gpu(D)
     L = gpuarray.zeros_like(M)
     S = gpuarray.zeros_like(M)    
     Y = gpuarray.zeros_like(M)
     print M.shape
+
     mu = (M.shape[0] * M.shape[1]) / (4.0 * L1Norm(M))
     lamb = max(M.shape) ** -0.5
+
     while not converged(M,L,S):
         L = svd_shrink(M - S - (mu**-1) * Y, mu)
         S = shrink(M - L + (mu**-1) * Y, lamb * mu)
@@ -50,9 +53,13 @@ def L1Norm(X):
 def converged(M, L, S):
     error = frobeniusNorm(M - L - S) / frobeniusNorm(M)
     print "error =", error
-    return error.get() <= 10e-6
+    return error <= 10e-6
 
 
-if __name__ == "main":
-    pass
+if __name__ == "__main__":
+    test = np.random.randint(0, 255, (100, 100))
+    L, S = robust_pca(test)
+    print L
+    print S
+
 
