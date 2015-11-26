97c275dc4f7d814dded7de52909ba9009b231788
1Reinier <reiniermaat@msn.com>
2015-11-26 14:09:47 -0500

    rpca cuda update

--- rpca_cuda.py
+++ rpca_cuda.py
@@ -60,7 +60,7 @@ def shrink(X, tau):
 
 def frobeniusNorm(X):
     Z = gpuarray.empty_like(X)
-    square(X, Z, X.size)
+    square(X, Z, X.size, block=(100,100))
     accum = gpuarray.sum(Z).get()
     print X.get()
     print Z.get()
