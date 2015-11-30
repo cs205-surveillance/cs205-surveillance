f7c4e2fc75b3b6b9393b23aa2ae35df9af5f31a4
Andrew Petschek <petschek@g.harvard.edu>
2015-11-30 15:15:30 -0500

    result

--- driver.py
+++ driver.py
@@ -54,5 +54,6 @@ run_super_pixel(OUT_gpu,TOL,OUT2, block=(15,15,1), grid=(1920/15,1080/15))
 result = OUT2.get().reshape((1920/15,1080/15))
 
 # Show image, perhaps with pylab
+print result
 plt.imshow(result)
 
