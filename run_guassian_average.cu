f589bb25f1520d38d7d00b28a338e9461e55f87d
Andrew Petschek <petschek@g.harvard.edu>
2015-11-30 14:46:26 -0500

    semicolons added to guass.cu

--- run_guassian_average.cu
+++ run_guassian_average.cu
@@ -7,25 +7,25 @@ __global void run_guassian_average(float *I, float *mu, float *sig2, float *OUT)
 	// OUT = output image with filtered values for each pixel [1 if foreground, 0 if background]
 
 	// rho is a temporal parameter, used when updating the mean and variance
-	float rho = 0.01
-	float threshold = 1.96
+	float rho = 0.01;
+	float threshold = 1.96;
 
 	// DO I NEED TO DECLARE 'OUT' as __shared__ ??
 
 	// Get current idx
-	int idx = threadIdx.x + blockIdx.x * blockDim.x
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
 	// Compare abs(I[idx]-mu[idx])/sig[idx] < thres
 	if(abs(I[idx]-mu[idx])/sig2[idx] < thres) {
 		// If True, mark OUT[idx] = 1
-		OUT[idx] = 1
+		OUT[idx] = 1;
 	}
 	else {
 		// Else, mark OUT[idx] = 0, adjust mean and variance
-		OUT[idx] = 0
-		float d = abs(I[idx]-mu[idx])
-		mu[idx] = rho * I[idx] + (1 - rho) * mu[idx]
-		sig2[idx] = d*d * rho + (1 - rho) * sig2[idx]
+		OUT[idx] = 0;
+		float d = abs(I[idx]-mu[idx]);
+		mu[idx] = rho * I[idx] + (1 - rho) * mu[idx];
+		sig2[idx] = d*d * rho + (1 - rho) * sig2[idx];
 	}
 
 }
