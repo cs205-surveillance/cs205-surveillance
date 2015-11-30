8c43f5fc1e116f7233e70c97686cba2dc6dec189
Andrew Petschek <petschek@g.harvard.edu>
2015-11-30 14:49:26 -0500

    __global__

--- run_guassian_average.cu
+++ run_guassian_average.cu
@@ -1,4 +1,4 @@
-__global void run_guassian_average(float *I, float *mu, float *sig2, float *OUT)
+__global__ void run_guassian_average(float *I, float *mu, float *sig2, float *OUT)
 {
 	// I = input image, intensities
 	// mu = running average intensity for each pixel, initially set to 1st image
