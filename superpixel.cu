0f2da61e6521f92d33a28d9a4e3f0b09de68c90e
Andrew Petschek <petschek@g.harvard.edu>
2015-11-30 14:23:58 -0500

    changed superpixel from 1d to 2d

--- superpixel.cu
+++ superpixel.cu
@@ -1,45 +1,41 @@
 __global__ void superPixel(int *inputs, float TOL, int *output)
 {
 	
-	//This kernel receives input values of 1's and 0's from gaussian running average/rPCA 
-	//This kernel sums each pixels (i.e., the 1's) in each workgroup (superpixel)
-	//This kernel outputs a single value (1/0) that represents if sum of values in each superpixel is above TOL
-
-	//NOTES:
-	//output is a 1 x [number of superpixels] array
-	//sum is a 1 x [number of threads per block (blockDim.x)] array
-	//input is a 1 x [number of pixels in image] array
-	//blockDim.x may have to be set manually to a power of 2
-	//need to set threshold per workgroup/block in driver to assess output values
-
-	//initialize indexing values
-	int idx = threadIdx.x + blockIdx.x * blockDim.x; //global threadId
-	int threadIdx = threadIdx.x;                     //local threadId in a block
-	int blockIdx = blockIdx.x;                       //block index
-	int blockDim = blockDim.x;                       //threads per block
+	//blockDim.x,y gives the number of threads in a block, in the particular direction
+	//gridDim.x,y gives the number of blocks in a grid, in the particular direction
+	//blockDim.x * gridDim.x gives the number of threads in a grid (in the x direction, in this case)
+	
+	//block id
+	int blockId = blockIdx.x + blockIdx.y * gridDim.x; 
+	
+	//global thread id
+	int globalId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
+	
+	//local thread id
+	int localId = (threadIdx.y * blockDim.x) + threadIdx.x                
 
 	//initialize local sum array to be filled in with values from our input array
-	__shared__ int sum[blockDim];
+	__shared__ int sum[gridDim.x*gridDim.y];
 
 	//assign values from binary input value array to our local sum array
-    sum[threadIdx] = inputs[idx]; 
+    sum[localId] = inputs[globalId]; 
     __syncthreads();
 
     //add up all 1's and 0's in local group using binary reduction
-	for (size_t offset = blockDim/2; offset > 0 ; offset >>= 1) {
-        if (threadIdx < offset) {    
-            sum[threadIdx] += sum[threadIdx + offset];
+	for (size_t offset = blockDim.x/2; offset > 0 ; offset >>= 1) {
+        if (localId < offset) {    
+            sum[localId] += sum[localId + offset];
         }
         __syncthreads();
     }
     //ouput final value
-    if (threadIdx == 0) {
-    	float percentOnes = sum[0]/blockDim;
+    if (localId == 0) {
+    	float percentOnes = sum[0]/(blockDim.x*blockDim.y);
 	    if (percentOnes > TOL) {
-	    	output[blockIdx] = 1;
+	    	output[blockId] = 1;
 	    }
 	    else {
-	    	output[blockIdx] = 0;
+	    	output[blockId] = 0;
 	    }
 	}
 }
