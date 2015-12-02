74782285af01943fa37df7bcf96fd74cd595afae
Taylor Killian <taylorkillian@g.harvard.edu>
2015-12-02 09:59:46 -0500

    starting to put serial implementation together

---
+++ serial_driver.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image, ImageDraw
+from scipy import misc
+from serial_run_gaussian_average import rga
+from superpixelserial import superPixel
+
+print "Made it through import statements"
+
