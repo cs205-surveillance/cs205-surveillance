fd5d0fa4d367edf771a849d198e1a58811ab691a
1Reinier <reiniermaat@msn.com>
2015-12-02 16:29:01 -0500

    Some more cleaning

--- run_gaussian_average.cu
+++ run_gaussian_average.cu
@@ -1,5 +1,4 @@
-__global__ void run_gaussian_average(float *I, float *mu, float *sig2, float *OUT)
-{
+__global__ void run_gaussian_average(float *I, float *mu, float *sig2, float *OUT) {
     /*
 	 I = input image, intensities
 	 mu = running average intensity for each pixel, initially set to 1st image
