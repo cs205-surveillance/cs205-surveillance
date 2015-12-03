83098f262a5cdfcff45582cad7e2d7d289c8834b
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 16:48:06 -0500

    editing comments

--- superpixel.cu
+++ superpixel.cu
@@ -9,12 +9,11 @@ __global__ void superPixel(float *inputs, int *output)
 
 	/*
 	This kernel is used to determine if there is an anomaly in a superpixel.
-	This kernel takes in a 1080x1920 continuous array and calculates the sum for each
-	30x30 pixel block (i.e., superpixel). Once the summation for every block is 
-	complete, the kernel calculates the fraction of the computed sum over the area of the 
-	whole superpixel. If that fraction is above our tolerance, the kernel returns a 1 for that superpixel.
-	Else, it returns a 0. The final output is a 1D array, where each value in that array corresponds to 
-	one of the superpixels.
+	This kernel takes in a 1080x1920 array (the output from minimum filter kernel) 
+	and calculates the sum for each 30x30 pixel block (i.e., superpixel). Once the 
+	summation for every block is complete, the kernel determines if the sum is above our
+	tolerance. If so, the kernel returns a 1 for that superpixel. Else, it returns a 0. The 
+	final output is a 1D array, where each value in that array corresponds to one of the superpixels.
 	*/ 
 
 	///////////////////////////
@@ -35,44 +34,33 @@ __global__ void superPixel(float *inputs, int *output)
 	int localId = (threadIdx.y * blockDim.x) + threadIdx.x;               
 
 
-	// Initialize local sum array to be filled in with values from our input array
+	// Initialize local array to be filled with values from input array
 	__shared__ float inputsToSum[30*30];
 
-	// Assign values from input value array to our local sum array
+	// Assign values from input value array to local sum array
     inputsToSum[localId] = inputs[globalId];
     __syncthreads();
 
 	/////////////////
 	// COMPUTATION //
 	/////////////////
-    
 
-    // First thread in each row will compute row sum
+    // First thread in each row will compute a row sum
 	if (localId % 30 == 0) {
-    	
     	for (int i = localId + 1; i < localId + 30; i++) {
-
     		inputsToSum[localId] += inputsToSum[i];
-    		
     	}
     }
-	
 	__syncthreads();
 
-	// One thread will combine above sums to get single value
+	// One thread will combine all above sums to get single value
 	if (localId == 0) {
     	for (int i=0; i<900; i+= 30) {
     		inputsToSum[0] += inputsToSum[i];
     	}
     }
 
-    // if (localId == 0 {
-    // 	for (int i=1; i<900; i++) {
-    // 		inputsToSum[0] = inputsToSum[0] + inputsToSum[i];
-    // 		__syncthreads();
-    // 	}
-    // }
-
+    // Assign 1 if sum above threshold. 
     if (localId == 0) {
         if (inputsToSum[0] > 700) { 
             output[blockId] = 1;
