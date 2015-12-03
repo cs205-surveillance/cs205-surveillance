8a0932470c0dd3264854e6fd23c92274785d32b4
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 00:56:29 -0500

    edits

--- superpixel.cu
+++ superpixel.cu
@@ -66,7 +66,7 @@ __global__ void superPixel(float *inputs, int *output)
     	printf("%d\n",globalId);
 		printf("%d\n",blockId);
 		printf("%d\n",localId);
-		printf("%d\n",inputsToSum[1]);
+		printf("%d\n",inputsToSum[0]);
     }
     //Add up all values in local group using binary reduction
 	// for (size_t offset = blockDim.x/2; offset > 0 ; offset >>= 1) {
