959b9e9675fd80d77122cb25d936a07ccd5b3a05
Andrew Petschek <petschek@g.harvard.edu>
2015-12-02 23:12:41 -0500

    edits

--- superpixel.cu
+++ superpixel.cu
@@ -62,7 +62,7 @@ __global__ void superPixel(float *inputs, int *output)
 
     //Ouput final value
     if (localId == 0) {
-    	float fraction = inputsToSum[0]/(blockDim.x*blockDim.y);
+    	float fraction = inputsToSum[0]/(900);
 	    if (fraction > .5) {
 	    	output[blockId] = 1; //inputs to sum
 	    }
