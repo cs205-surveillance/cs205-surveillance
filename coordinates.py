83a214970b255657530dff714f1e3bb9fdc83e65
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 17:08:21 -0500

    divide by 30

--- coordinates.py
+++ coordinates.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 def coordinates(inputs):
 
     # Reshape input array to 2D
-    inputs = inputs.reshape(1080,1920)
+    inputs = inputs.reshape(1080/30,1920/30)
     
     # Determine corresponding top-left corner pixel location for superpixels with 1's
     output = []
-    for i in range(1080):
-        for j in range(1920):
+    for i in range(1080/30):
+        for j in range(1920/30):
             if inputs[i,j]==1:
                 point = [i*30,j*30]
                 output.append(point)
