c23ba8586216d7d952def8a385280f7df300c2c9
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 08:59:07 -0500

    edits

--- testsuperpixel.cu
+++ testsuperpixel.cu
@@ -4,8 +4,12 @@
 __global__ void testsuperPixel(int *inputs, int *output)
 {
 	int blockId = blockIdx.x + blockIdx.y * gridDim.x; 
-	int globalId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
+	//int globalId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
 	int localId = (threadIdx.y * blockDim.x) + threadIdx.x;              
+    int globalIdX = blockIdx.x * blockDim.x + threadIdx.x;
+    int globalIdY = blockIdx.y * blockDim.y + threadIdx.y;
+    int globalId = (globalIdY * 8) + globalIdX;
+
 
 	// Initialize local sum array to be filled in with values from our input array
 	__shared__ int inputsToSum[16];
