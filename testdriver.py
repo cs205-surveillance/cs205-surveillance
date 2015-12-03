55d81e29746775aad9c06cdd4ec02dd0a078de82
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 09:12:19 -0500

    edits

--- testdriver.py
+++ testdriver.py
@@ -20,7 +20,7 @@ spxl_out = np.zeros(4, dtype=int)
 spxl_out_gpu = gpuarray.to_gpu(spxl_out)
 
 # Run super pixel kernel
-run_super_pixel(test_array_gpu, spxl_out_gpu, block=(2, 2, 1), grid=(16, 8))
+run_super_pixel(test_array_gpu, spxl_out_gpu, block=(4, 4, 1), grid=(16, 8))
 result = spxl_out_gpu.get()#.reshape((8,16))
 
 # Show image, perhaps with pylab
