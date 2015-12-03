9f3283e2c6488f97fc37e12b2f53e0a780406892
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 10:14:22 -0500

    test

--- testdriver.py
+++ testdriver.py
@@ -9,15 +9,16 @@ from pycuda.compiler import SourceModule
 test_superpixel_source = SourceModule(open('testsuperpixel.cu').read())
 run_super_pixel = test_superpixel_source.get_function('testsuperPixel')
 
-test_array = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2]])
+#test_array = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2],[0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2]])
+test_array = np.array([[0,0,1,1],[0,0,2,2]])
 test_array_gpu = gpuarray.to_gpu(test_array)
 print test_array
 
-spxl_out = np.zeros(8, dtype=int)
+spxl_out = np.zeros(2, dtype=int)
 spxl_out_gpu = gpuarray.to_gpu(spxl_out)
 
 # Run super pixel kernel
-run_super_pixel(test_array_gpu, spxl_out_gpu, block=(4, 4, 1))
+run_super_pixel(test_array_gpu, spxl_out_gpu, block=(2, 2, 1), grid=(1,2))
 result = spxl_out_gpu.get()#.reshape((8,16))
 
 # Show image, perhaps with pylab
