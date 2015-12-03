8cf2cdf65aa6da337d970ca5083b42a290e2a79d
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 12:08:57 -0500

    test

--- superpixel.cu
+++ superpixel.cu
@@ -52,7 +52,7 @@ __global__ void superPixel(float *inputs, int *output)
     }
     __syncthreads();
     if (localId == 0) {
-        if (inputsToSum[0] > 700) { 
+        if (inputsToSum[0] > 200) { 
             output[blockId] = 1;
         }  
         else{
