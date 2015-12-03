93d60f40df90bee2c71e936f77e3f51795d38e27
1Reinier <reiniermaat@msn.com>
2015-12-03 17:22:03 -0500

    Min filter work

--- serial_min_filter.py
+++ serial_min_filter.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+
 def min_filter(image, iterations=10):
-    ''' filter using numpy '''
+    """ filter using numpy """
     for i in range(iterations):
         padded = np.pad(image, 1, mode='edge')
         stacked = np.dstack((padded[:-2,  :-2], padded[:-2,  1:-1], padded[:-2,  2:],
