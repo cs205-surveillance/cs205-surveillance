45a49f5ada1a2da1cc540403e7525e2a2288bf52
1Reinier <reiniermaat@msn.com>
2015-12-02 20:00:38 -0500

    Driver update

--- driver.py
+++ driver.py
@@ -48,9 +48,9 @@ for i in range(65, 90):
     rga_out_gpu = rga_out_gpu.reshape((1080, 1920))
 
     # Copy back (for testing)
-    #sig2_result = sig2_gpu.get()
-    #mu_result = mu_gpu.get().reshape((1080, 1920))
-    #rga_result = rga_out_gpu.get().reshape((1080, 1920))
+    # sig2_result = sig2_gpu.get()
+    # mu_result = mu_gpu.get().reshape((1080, 1920))
+    # rga_result = rga_out_gpu.get().reshape((1080, 1920))
 
     # plt.imshow(rga_result)
     # plt.show()
@@ -61,12 +61,12 @@ for i in range(65, 90):
     # Set parameters for super pixel kernel
     tol = np.array([.75])
     tol_gpu = gpuarray.to_gpu(tol)
-    spxl_out = np.zeros((1920/30)*(1080/30),dtype=int)
+    spxl_out = np.zeros((1920/30) * (1080/30), dtype=int)
     spxl_out_gpu = gpuarray.to_gpu(spxl_out)
     
     # Run super pixel kernel
-    run_super_pixel(rga_out_gpu,tol_gpu, spxl_out_gpu, block=(30,30,1), grid=(1080/30,1920/30))
-    result = spxl_out_gpu.get().reshape((1080/30,1920/30))
+    run_super_pixel(rga_out_gpu,tol_gpu, spxl_out_gpu, block=(30, 30, 1), grid=(1080/30, 1920/30))
+    result = spxl_out_gpu.get().reshape((1080/30, 1920/30))
 
     print result
     
