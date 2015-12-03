4eac9818d09968d8819a5b4d1a26acc4a72fef67
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 10:49:11 -0500

    test

--- testsuperpixel.cu
+++ testsuperpixel.cu
@@ -15,7 +15,7 @@ __global__ void testsuperPixel(int *inputs, int *output)
     __shared__ int inputsToSum[8];
     inputsToSum[localId] = inputs[globalId];
 
-    if (globalId == 7) {
+    if (localId == 0) {
         printf("%d",inputsToSum[localId]);
 
     }
