e426c16e03b45a1fa66d92015ea8e198d6677bfd
Andrew Petschek <petschek@g.harvard.edu>
2015-12-02 21:30:34 -0500

    edits

--- superpixel.cu
+++ superpixel.cu
@@ -37,7 +37,7 @@ __global__ void superPixel(float *inputs, float *TOL, float *output)
 	int localId = (threadIdx.y * blockDim.x) + threadIdx.x;              
 
 	// Initialize local sum array to be filled in with values from our input array
-	//__shared__ int sum[30*30];
+	__shared__ float sum[30*30];
 
 	// Assign values from input value array to our local sum array
     //sum[localId] = inputs[globalId]; 
@@ -49,29 +49,29 @@ __global__ void superPixel(float *inputs, float *TOL, float *output)
 
     // if (localId == 0) {
     // 	for (int i = 0; i < 30*30; i++) {
-    output[globalId] = inputs[globalId];
+    //output[globalId] = inputs[globalId];
     //	}
     //}
-    __syncthreads();
+    //__syncthreads();
 
     //Add up all values in local group using binary reduction
-	// for (size_t offset = blockDim.x/2; offset > 0 ; offset >>= 1) {
- //        if (localId < offset) {    
- //            sum[localId] += sum[localId + offset];
- //        }
- //        __syncthreads();
- //    }
+	for (size_t offset = blockDim.x/2; offset > 0 ; offset >>= 1) {
+        if (localId < offset) {    
+            sum[localId] += sum[localId + offset];
+        }
+        __syncthreads();
+    }
     
-    // Ouput final value
- //    if (localId == 0) {
- //    	float fraction = sum[0]/(blockDim.x*blockDim.y);
-	//     if (fraction > TOL[0]) {
-	//     	output[blockId] = 1;
-	//     }
-	//     else {
-	//     	output[blockId] = 0;
-	//     }
-	// }
-	// __syncthreads();
+    //Ouput final value
+    if (localId == 0) {
+    	float fraction = sum[0]/(blockDim.x*blockDim.y);
+	    if (fraction > TOL[0]) {
+	    	output[blockId] = 1;
+	    }
+	    else {
+	    	output[blockId] = 0;
+	    }
+	}
+	__syncthreads();
 }
 
