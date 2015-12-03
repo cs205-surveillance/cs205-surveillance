043d30804961e51160489b40532b6e1d69cae793
Andrew Petschek <petschek@g.harvard.edu>
2015-12-03 10:47:55 -0500

    test

--- testsuperpixel.cu
+++ testsuperpixel.cu
@@ -12,9 +12,11 @@ __global__ void testsuperPixel(int *inputs, int *output)
 	//int globalId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
 	int localId = (threadIdx.y * blockDim.x) + threadIdx.x;              
     
+    __shared__ int inputsToSum[8]
+    inputsToSum[localId] = inputs[globalId];
 
     if (globalId == 4) {
-        printf("%d",inputs[globalIdX,globalIdY]);
+        printf("%d",inputsToSum[localId]);
 
     }
 
