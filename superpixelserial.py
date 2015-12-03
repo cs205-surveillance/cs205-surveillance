6b019e23a9b99cec9140d1a85c544bb8a60f88f6
Andrew Petschek <petschek@g.harvard.edu>
2015-12-02 19:05:55 -0500

    we are now PEP8 COMPLIANT

--- superpixelserial.py
+++ superpixelserial.py
@@ -12,7 +12,7 @@ def superPixel(incoords,TOL,width=15,height=15):
     The fct takes in a 2D binary array and computes parameters re: the img.
     4 for loops are required to loop in 2 dimensions through the
     pixels within an individual superpixel and loop in 2 dimensions
-    through the superpixels themselves within the input array
+    through the superpixel themselves within the input array
     
     #########
     # Input #
@@ -36,73 +36,50 @@ def superPixel(incoords,TOL,width=15,height=15):
     """
 
     #image dimensions in terms of pixels
-    imgWidth = incoords.shape[1]
-    imgHeight = incoords.shape[0]
+    img_width = incoords.shape[1]
+    img_height = incoords.shape[0]
 
     #single superpixel width and height in terms of pixels
-    blockDimC = width   
-    blockDimR = height 
+    block_dim_c = width   
+    block_dim_r = height 
 
-    #image dimensions in terms of superpixels
-    gridDimC = imgWidth/blockDimC
-    gridDimR = imgHeight/blockDimR
+    #image dimensions in terms of super_pixel
+    grid_dim_c = img_width/block_dim_c
+    grid_dim_r = img_height/block_dim_r
 
-    #number of superpixels in image
-    N = gridDimC*gridDimR
-    superPixels = np.zeros(N)
+    #number of super_pixel in image
+    N = grid_dim_c*grid_dim_r
+    super_pixel = np.zeros(N)
     
     #loop and calculate sum of 1's in each superpixel
     n=0
-    for r in range(gridDimR):
-        for c in range(gridDimC):
-            for i in range(blockDimR):
-                for j in range(blockDimC):
-                    superPixels[n] += incoords[i+blockDimR*r,j+blockDimC*c]
+    for r in range(grid_dim_r):
+        for c in range(grid_dim_c):
+            for i in range(block_dim_r):
+                for j in range(block_dim_c):
+                    super_pixel[n] += incoords[i+block_dim_r*r,j+block_dim_c*c]
             n+=1
     
     #calculate percent of ones in each superpixel
-    superPixels /= (blockDimR*blockDimC)        
+    super_pixel /= (block_dim_r*block_dim_c)        
     
     #assign 1 if percent > TOL, else assign 0
-    outputA = np.zeros(N)
-    for i in range(len(superPixels)):
-        if superPixels[i] > TOL:
-            outputA[i] = 1
+    output_intermediate = np.zeros(N)
+    for i in range(len(super_pixel)):
+        if super_pixel[i] > TOL:
+            output_intermediate[i] = 1
     
-    outputA = outputA.reshape(gridDimR,gridDimC)
+    output_intermediate = output_intermediate.reshape(grid_dim_r,grid_dim_c)
     
-    #take our grid of superpixels and output corresponding top-left corner pixel location
-    outputB = []
-    for i in range(gridDimR):
-        for j in range(gridDimC):
-            if outputA[i,j]==1:
-                point = [i*blockDimR,j*blockDimC]
-                outputB.append(point)
+    #take our grid of super_pixel and output corresponding top-left corner pixel location
+    output_final = []
+    for i in range(grid_dim_r):
+        for j in range(grid_dim_c):
+            if output_intermediate[i,j]==1:
+                point = [i*block_dim_r,j*block_dim_c]
+                output_final.append(point)
 
-    return outputB
-
-"""
-########
-# TEST #
-########
-
-#loop through set of images
-for i in range(65,89):
-    image_number = str(i)
-    while len(image_number) < 3:
-        image_number = '0' + image_number
-
-    #load image
-    I = misc.imread('cs205_images/Output/out{}.png'.format(image_number), flatten=True)
-    I = I.astype(np.float32)
-
-    #run superPixel fct
-    output = superPixel(I,.8,30,30)
-
-    #plot resulting output of superpixels
-    plt.imshow(output)
-    plt.show()
-"""
+    return output_final
 
 
 
