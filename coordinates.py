a4b1fdb534f0fa60c7e1f1004992d46e15a63a96
Andrew Petschek <petschek@g.harvard.edu>
2015-12-04 17:41:55 -0500

    edits

--- coordinates.py
+++ coordinates.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 def coordinates(inputs):
 
     # Reshape input array to 2D
-    inputs = inputs.reshape(1080/30,1920/30)
+    inputs = inputs.reshape(1080/30,1920/32)
     
     # Determine corresponding top-left corner pixel location for superpixels with 1's
     output = []
     for i in range(1080/30):
-        for j in range(1920/30):
+        for j in range(1920/32):
             if inputs[i,j]==1:
-                point = [i*30,j*30]
+                point = [i*30,j*32]
                 output.append(point)
 
     return output
