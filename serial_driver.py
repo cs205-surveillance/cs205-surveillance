369bbef004d53943b69f4c2d5269c571c17aa1f4
Taylor Killian <taylorkillian@g.harvard.edu>
2015-12-03 21:14:06 -0500

    Created various save statements to pull off step-by-step images

--- serial_driver.py
+++ serial_driver.py
@@ -11,10 +11,9 @@ from serial_min_filter import min_filter
 print "Made it through import statements"
 
 r = 30 # pixels, used to define superpixel dimension and outline
+time_array = []
 
-# global mu, sig2
 # Read in sequence of images, run them through RGA filter
-
 for i in range(69,300):
 	image_number = str(i)
 	while len(image_number) < 3:
@@ -28,20 +27,26 @@ for i in range(69,300):
 		mu = np.copy(I)
 		sig2 = np.ones_like(I)
 
-
 	OUT, mu, sig2 = rga(I, 2.5, mu, sig2, cont=True)
-	# print "Number of non-zero entries: ", np.count_nonzero(OUT)
-	# plt.imshow(OUT)
-	# plt.show()
+	
+	# Save output from RGA
+	misc.imsave("cs205_images/mu_stack/mu_{}.jpeg".format(image_number), mu)
+	misc.imsave("cs205_images/sig2_stack/sig2_{}.jpeg".format(image_number), sig2)
+	misc.imsave("cs205_images/cont_output/cont_{}.jpeg".format(image_number), OUT)
 
 	filt_out = min_filter(OUT, iterations=1)
 
-	# misc.imsave('cs205_images/cont_output/cont_out{}.png'.format(image_number),OUT)
-	superpixel_output = superPixel(filt_out, 10*700,r,r)
+	# Save output from 3x3 minimum filter
+	misc.imsave("cs205_images/filtered_output/filt_{}.jpeg".format(image_number), filt_out)
+
+	superpixel_output = superPixel(filt_out, 12.5*700,r,r)
 	output = coordinates(superpixel_output)
 
-    t1 = time()
-    time_array.append(t1-t0)
+
+	misc.imsave("cs205_images/superpixel_stack/sp_{}.jpeg".format(image_number), superpixel_output.reshape(1080/30.,1920/30.))
+
+	t1 = time()
+	time_array.append(t1-t0)
 
 	im = Image.open('cs205_images/grabber{}.ppm'.format(image_number))
 	draw = ImageDraw.Draw(im)
@@ -56,6 +61,6 @@ for i in range(69,300):
 			draw.line((pt[1]+r, pt[0], pt[1]+r, pt[0]+r), fill=(255,120,0), width=4)
 
 	del draw
-	im.save('cs205_images/serial_output/tracker{}.png'.format(image_number))
+	im.save('cs205_images/serial_output/tracker_{}.png'.format(image_number))
 
 print "Per frame processing time: ", np.mean(time_array)
