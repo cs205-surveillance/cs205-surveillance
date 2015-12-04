c133c30b8399b9d065920d6085cfbc64aa55bda5
Taylor Killian <taylorkillian@g.harvard.edu>
2015-12-03 22:14:52 -0500

    Slight changes to serial_driver, saving all jpegs

--- serial_driver.py
+++ serial_driver.py
@@ -30,20 +30,20 @@ for i in range(69,300):
 	OUT, mu, sig2 = rga(I, 2.5, mu, sig2, cont=True)
 	
 	# Save output from RGA
-	misc.imsave("cs205_images/mu_stack/mu_{}.jpeg".format(image_number), mu)
-	misc.imsave("cs205_images/sig2_stack/sig2_{}.jpeg".format(image_number), sig2)
-	misc.imsave("cs205_images/cont_output/cont_{}.jpeg".format(image_number), OUT)
+	# misc.imsave("cs205_images/mu_stack/mu_{}.jpeg".format(image_number), mu)
+	# misc.imsave("cs205_images/sig2_stack/sig2_{}.jpeg".format(image_number), sig2)
+	# misc.imsave("cs205_images/cont_output/cont_{}.jpeg".format(image_number), OUT)
 
 	filt_out = min_filter(OUT, iterations=1)
 
 	# Save output from 3x3 minimum filter
-	misc.imsave("cs205_images/filtered_output/filt_{}.jpeg".format(image_number), filt_out)
+	# misc.imsave("cs205_images/filtered_output/filt_{}.jpeg".format(image_number), filt_out)
 
 	superpixel_output = superPixel(filt_out, 12.5*700,r,r)
 	output = coordinates(superpixel_output)
 
 
-	misc.imsave("cs205_images/superpixel_stack/sp_{}.jpeg".format(image_number), superpixel_output.reshape(1080/30.,1920/30.))
+	# misc.imsave("cs205_images/superpixel_stack/sp_{}.jpeg".format(image_number), superpixel_output.reshape(1080/30.,1920/30.))
 
 	t1 = time()
 	time_array.append(t1-t0)
@@ -61,6 +61,6 @@ for i in range(69,300):
 			draw.line((pt[1]+r, pt[0], pt[1]+r, pt[0]+r), fill=(255,120,0), width=4)
 
 	del draw
-	im.save('cs205_images/serial_output/tracker_{}.png'.format(image_number))
+	im.save('cs205_images/serial_output/tracker_{}.jpeg'.format(image_number))
 
 print "Per frame processing time: ", np.mean(time_array)
