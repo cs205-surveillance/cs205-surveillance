7fb5a2cde3c0b9c6f4dc6139b69f3b52f45e5456
Taylor Killian <taylorkillian@g.harvard.edu>
2015-12-05 01:03:58 -0500

    Cleaning up repo

--- Serial Implementation/serial_driver.py
+++ Serial Implementation/serial_driver.py
@@ -11,6 +11,9 @@ from serial_min_filter import min_filter
 print "Made it through import statements"
 
 r = 30 # pixels, used to define superpixel dimension and outline
+time_array_rga = []
+time_array_min = []
+time_array_sup = []
 time_array = []
 
 # Read in sequence of images, run them through RGA filter
@@ -28,6 +31,7 @@ for i in range(69,300):
 		sig2 = np.ones_like(I)
 
 	OUT, mu, sig2 = rga(I, 2.5, mu, sig2, cont=True)
+	t1 = time()
 	
 	# Save output from RGA
 	# misc.imsave("cs205_images/mu_stack/mu_{}.jpeg".format(image_number), mu)
@@ -35,32 +39,40 @@ for i in range(69,300):
 	# misc.imsave("cs205_images/cont_output/cont_{}.jpeg".format(image_number), OUT)
 
 	filt_out = min_filter(OUT, iterations=1)
+	t2 = time()
 
 	# Save output from 3x3 minimum filter
 	# misc.imsave("cs205_images/filtered_output/filt_{}.jpeg".format(image_number), filt_out)
 
 	superpixel_output = superPixel(filt_out, 12.5*700,r,r)
-	output = coordinates(superpixel_output)
+	t3 = time()
+	# output = coordinates(superpixel_output)
 
 
 	# misc.imsave("cs205_images/superpixel_stack/sp_{}.jpeg".format(image_number), superpixel_output.reshape(1080/30.,1920/30.))
 
-	t1 = time()
-	time_array.append(t1-t0)
+	time_array.append(t3-t0)
+	time_array_rga.append(t1-t0)
+	time_array_min.append(t2-t1)
+	time_array_sup.append(t3-t2)
 
-	im = Image.open('cs205_images/grabber{}.ppm'.format(image_number))
-	draw = ImageDraw.Draw(im)
-	numAnom = len(output)
+	# im = Image.open('cs205_images/grabber{}.ppm'.format(image_number))
+	# draw = ImageDraw.Draw(im)
+	# numAnom = len(output)
 	
-	if numAnom > 0:
-		for pt in output:
-			#Draw rectangles
-			draw.line((pt[1],pt[0],pt[1],pt[0]+r), fill=(255,120,0), width=4)
-			draw.line((pt[1], pt[0]+r, pt[1]+r, pt[0]+r), fill=(255,120,0), width=4)
-			draw.line((pt[1], pt[0], pt[1]+r, pt[0]), fill=(255,120,0), width=4)
-			draw.line((pt[1]+r, pt[0], pt[1]+r, pt[0]+r), fill=(255,120,0), width=4)
+	# if numAnom > 0:
+	# 	for pt in output:
+	# 		#Draw rectangles
+	# 		draw.line((pt[1],pt[0],pt[1],pt[0]+r), fill=(255,120,0), width=4)
+	# 		draw.line((pt[1], pt[0]+r, pt[1]+r, pt[0]+r), fill=(255,120,0), width=4)
+	# 		draw.line((pt[1], pt[0], pt[1]+r, pt[0]), fill=(255,120,0), width=4)
+	# 		draw.line((pt[1]+r, pt[0], pt[1]+r, pt[0]+r), fill=(255,120,0), width=4)
 
-	del draw
-	im.save('cs205_images/serial_output/tracker_{}.jpeg'.format(image_number))
+	# del draw
+	# im.save('cs205_images/serial_output/tracker_{}.jpeg'.format(image_number))
 
 print "Per frame processing time: ", np.mean(time_array)
+print "Per frame rga time: ", np.mean(time_array_rga)
+print "Per frame min_filt time: ", np.mean(time_array_min)
+print "Per frame superpixel time: ", np.mean(time_array_sup)
+
