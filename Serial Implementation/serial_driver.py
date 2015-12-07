3694f6d13e5a7271ff4356018edf0b264d36c188
1Reinier <reiniermaat@msn.com>
2015-12-06 19:33:39 -0500

    Removed print statements

--- Serial Implementation/serial_driver.py
+++ Serial Implementation/serial_driver.py
@@ -52,7 +52,6 @@ for i in range(260, 644):
     # Aggregate into superpixels and flag anomalous behavior
     superpixel_output = super_pixel(filt_out, 12.5 * 700, r0, r1)
     t3 = time()
-    print superpixel_output.shape, superpixel_output.size
     output = coordinates(superpixel_output)
 
     # Track how much time each module took.
